d987d2d955ab9f557f42d60a6bdbd411f4523b0d
Larry Green <lfgreen3@gmail.com>
2018-03-03 12:19:26 -0600

    Update create_new_package to update strings inside of the scripts

--- README.rst
+++ README.rst
@@ -3,9 +3,14 @@ Python Package Boilerplate
 
 A simple boilerplate template for python packages.
 
+Requirements
+------------
 
-Usage
------
+* pytest
+
+
+Installation
+------------
 
 If you want to use this one time, then just clone the package and remove the create_new_package.sh
 
@@ -20,5 +25,20 @@ then save this project somewhere for re-use.
 .. code-block:: sh
 
    git clone https://github.com/larrygreen3/python-package-boilerplate.git python-package-boilerplate
-   
+
    ./python-package-boilerplate/create_new_package.sh PACKAGE_NAME
+
+
+Run Tests
+---------
+
+.. code-block:: sh
+
+   pytest
+
+
+Build
+-----
+.. code-block:: sh
+
+   ./build.sh
--- create_new_package.sh
+++ create_new_package.sh
@@ -8,11 +8,20 @@ then
   echo "Creating a new project \"$package_name\""
   rsync -rq --progress "$script_directory/" "./$package_name" --exclude ".git"
 
+  echo "Renaming folders/files"
+  mv "./$package_name/PACKAGENAME" "./$package_name/$package_name"
+  mv "./$package_name/tests/test_PACKAGENAME.py" "./$package_name/tests/test_$1.py"
+
   echo "Removing all objects inside the new package build directory"
   rm ./$package_name/build/*
 
   echo "Removing the create_new_package.sh from the new package directory"
   rm ./$package_name/create_new_package.sh
+
+  echo "Replacing all instances of \"PACKAGENAME\" with \"$1\""
+  sed -i 's/PACKAGENAME/'$1'/g' ./$package_name/setup.py
+  sed -i 's/PACKAGENAME/'$1'/g' ./$package_name/tests/test_$1.py
+
 else
   echo "Please enter a new package name.     ./create_new_package.sh NEW_PACKAGE_NAME"
 fi
